eb8ed54b848026de04b91b6b9cb7d7f709cb1120
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-03-08 11:46:47 +0530

    Update data_utils1.py

--- data_utils1.py
+++ data_utils1.py
@@ -33,7 +33,7 @@ def build_word_dict():
     if not os.path.exists("word_dict.pickle"):
         train_df = pd.read_csv(TRAIN_PATH, names=["class", "title", "content"])
         train_df_snli=pd.read_csv("/content/gdrive/My Drive/snli_1.0_train.txt",names=["sentence1","sentence2","gold_label"])
-        train_df_sts=pd.read_csv("/content/gdrive/My Drive/sts.csv",usecols=["score","sent1","sent2"])
+        train_df_sts=pd.read_csv("/content/gdrive/My Drive/sts.csv",usecols=["Score","Sent1","Sent2"])
         train_df_cola=pd.read_csv("/content/gdrive/My Drive/cola_public/raw/in_domain_train.tsv")
         
         contents = train_df["content"]
