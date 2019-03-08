f838d4711f2e5bc84190f7e4a79163a96c6ae4f7
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-03-08 12:23:29 +0530

    Update data_utils1.py

--- data_utils1.py
+++ data_utils1.py
@@ -74,14 +74,14 @@ def build_dataset(step, word_dict, max_document_len):
     if step == "train":
         df = pd.read_csv(TRAIN_PATH, names=["class", "title", "content"])
         df_snli=pd.read_csv("/content/gdrive/My_drive/snli_1.0_train.txt",names=["sentence1","sentence2","gold_label"])
-        df_sts=pd.read_csv("/content/gdrive/My_drive/sts.csv",sep='\t',usecols=["Score","Sent1","Sent2"])
-        df_cola = pd.read_csv("/content/gdrive/My_drive/cola_public/raw/in_domain_train.csv")
+        df_sts=pd.read_csv("/content/gdrive/My drive/sts.csv",sep='\t',usecols=["Score","Sent1","Sent2"])
+        df_cola = pd.read_csv("/content/gdrive/My drive/cola_public/raw/in_domain_train.csv")
        
     else:
         df=pd.read_csv(TEST_PATH, names=["class", "title", "content"])
         df_snli=pd.read_csv("snli_1.0/snli_1.0_test.csv",names=["sentence1","sentence2","gold_label"])
         df_sts=pd.read_csv("sts-test.csv",usecols=["score","sent1","sent2"])
-        df_cola = pd.read_csv("/content/gdrive/My_drive/cola_public/raw/in_domain_train.csv")
+        df_cola = pd.read_csv("/content/gdrive/My drive/cola_public/raw/in_domain_train.csv")
         
     df_sts['score'].replace('', np.nan, inplace=True)
     df_sts=df_sts.dropna(subset=['score'], inplace=True)    
