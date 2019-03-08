0b95901ec4deb3686d3295489cb8cf5297692fc3
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-03-08 09:43:16 +0530

    Update data_utils1.py

--- data_utils1.py
+++ data_utils1.py
@@ -32,13 +32,16 @@ def clean_str(text):
 def build_word_dict():
     if not os.path.exists("word_dict.pickle"):
         train_df = pd.read_csv(TRAIN_PATH, names=["class", "title", "content"])
-        train_df_snli=pd.read_csv("snli_1.0/snli_1.0_train.csv",names=["sentence1","sentence2","gold_label"])
-        train_df_sts=pd.read_csv("snlitrain.csv",names=["sentence1","sentence2","gold_label"])
+        train_df_snli=pd.read_csv("/content/gdrive/My Drive/snli_1.0/snli_1.0_train.csv",names=["sentence1","sentence2","gold_label"])
+        train_df_sts=pd.read_csv("/content/gdrive/My Drive/sts_train.csv")
+        train_df_cola=pd.read_csv("/content/gdrive/My Drive/cola_public/raw/in_domain_train.tsv")
+        
         contents = train_df["content"]
         contents.append(train_df_snli["sentence1"])
         contents.append(train_df_sts["sentence1"])
         contents.append(train_df_snli["sentence2"])
         contents.append(train_df_sts["sentence2"])
+        contents.append(train_df_cola[""])
        
         words = list()
         for content in contents:
@@ -68,19 +71,22 @@ def build_word_dict():
 def build_dataset(step, word_dict, max_document_len):
     if step == "train":
         df = pd.read_csv(TRAIN_PATH, names=["class", "title", "content"])
-        df_snli=pd.read_csv("snli_1.0/snli_1.0_train.csv",names=["sentence1","sentence2","gold_label"])
-        df_sts=pd.read_csv("sts-train.csv",names=["sentence1","sentence2","gold_label"])
-        df_cola = pd.read_csv("cola_public/raw/in_domain_train.csv",names=["sentence1","sentence2","gold_label"])
+        df_snli=pd.read_csv("/content/gdrive/My_drive/snli_1.0/snli_1.0_train.csv",names=["sentence1","sentence2","gold_label"])
+        df_sts=pd.read_csv("/content/gdrive/My_drive/sts.csv",usecols=["score","sent1","sent2"])
+        df_cola = pd.read_csv("/content/gdrive/My_drive/cola_public/raw/in_domain_train.csv")
        
     else:
-        df = pd.read_csv(TEST_PATH, names=["class", "title", "content"])
+        df=pd.read_csv(TEST_PATH, names=["class", "title", "content"])
         df_snli=pd.read_csv("snli_1.0/snli_1.0_test.csv",names=["sentence1","sentence2","gold_label"])
-        df_sts=pd.read_csv("sts-test.csv",names=["sentence1","sentence2","gold_label"])
-        
+        df_sts=pd.read_csv("sts-test.csv",usecols=["score","sent1","sent2"])
+        df_cola = pd.read_csv("/content/gdrive/My_drive/cola_public/raw/in_domain_train.csv")
+    df_sts['score'].replace('', np.nan, inplace=True)
+    df_sts=df_sts.dropna(subset=['score'], inplace=True)    
     # Shuffle dataframe
     df = df.sample(frac=1)
     df_snli = df_snli.sample(frac=1)
     df_sts = df_sts.sample(frac=1)
+    df_cola = df_cola.sample(frac=1)
 
     data = list(map(lambda d: word_tokenize(clean_str(d)), df["content"]))
     x = list(map(lambda d: ["<s>"] + d, data))
@@ -100,13 +106,13 @@ def build_dataset(step, word_dict, max_document_len):
     xnli2 = list(map(lambda d: d[:max_document_len], x))
     xnli2 = list(map(lambda d: d + (max_document_len - len(d)) * [word_dict["<pad>"]], x))
     
-    xsts1  list(map(lambda d: word_tokenize(clean_str(d)), df_sts["sentence1"]))
+    xsts1  list(map(lambda d: word_tokenize(clean_str(d)), df_sts["sent1"]))
     xsts1 = list(map(lambda d: ["<s>"] + d, data))
     xsts1 = list(map(lambda d: list(map(lambda w: word_dict.get(w, word_dict["<unk>"]), d)), x))
     xsts1 = list(map(lambda d: d[:max_document_len], x))
     xsts1 = list(map(lambda d: d + (max_document_len - len(d)) * [word_dict["<pad>"]], x))
     
-    xsts2 = list(map(lambda d: word_tokenize(clean_str(d)), df_sts["sentence2"]))
+    xsts2 = list(map(lambda d: word_tokenize(clean_str(d)), df_sts["sent2"]))
     xsts2 = list(map(lambda d: ["<s>"] + d, data))
     xsts2 = list(map(lambda d: list(map(lambda w: word_dict.get(w, word_dict["<unk>"]), d)), x))
     xsts2 = list(map(lambda d: d[:max_document_len], x))
@@ -123,15 +129,15 @@ def build_dataset(step, word_dict, max_document_len):
     lm_y = list(map(lambda d: d[:max_document_len], lm_y))
     lm_y = list(map(lambda d: d + (max_document_len - len(d)) * [word_dict["<pad>"]], lm_y))
     
-    clf_nli = list(map(lambda d: d - 1, list(df_nli["gold_label"])))
+    clf_nli = list(map(lambda d: d , list(pd.Categorical(df_nli["gold_label"]).cat.codes))
     
-    clf_sts = list(map(lambda d: d - 1, list(df_sts[""])))
+    clf_sts = list(map(lambda d: d, list(df_sts["score"])))
     
     clf_cola = list(map(lambda d: d, list(df_cola.iloc[:,1])))
 
     clf_y = list(map(lambda d: d - 1, list(df["class"])))
 
-    return x, xnli1, xnli2, xsts1, xsts2, lm_y, clf_y, clf_sts, clf_nli
+    return x, xnli1, xnli2, xsts1, xsts2, xcola, lm_y, clf_y, clf_sts, clf_nli
 
 
 def batch_iter(inputs, lm_outputs, clf_outputs, batch_size, num_epochs):
