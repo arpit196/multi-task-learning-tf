34995a56d61f768575eceec3a455c35030f115d8
arpit196 <30366755+arpit196@users.noreply.github.com>
2019-03-17 11:27:16 +0530

    Update train1.py

--- train1.py
+++ train1.py
@@ -6,7 +6,7 @@ from model1 import Model
 from data_utils1 import download_dbpedia, build_word_dict, build_dataset, batch_iter
 
 
-def train(train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y, train_clf_sts, train_clf_nli, test_x, test_xnli1, test_xnli2, test_xsts1, test_xsts2, test_lm_y, test_clf_y, test_clf_nli, test_clf_sts, word_dict, args):
+def train(train_x, train_cola, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y, train_clf_sts, train_clf_nli,train_clf_cola, test_x, test_xnli1, test_xnli2, test_xsts1, test_xsts2, test_lm_y, test_clf_y, test_clf_nli, test_clf_sts, word_dict, args):
     with tf.Session() as sess:
         model = Model(len(word_dict), num_class=14, args=args)
 
@@ -31,9 +31,9 @@ def train(train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_
         sess.run(tf.global_variables_initializer())
 
         def train_step(batch_x, batch_xnli1, batch_xnli2, batch_xsts1, batch_xsts2, batch_lm_y, batch_clf_y, batch_clf_sts, batch_clf_nli):
-            feed_dict = {model.x: batch_x, model.xnli11: batch_xnli1, model.xnli2:batch_xnli2, model.clf_nli:batch_clf_nli, model.clf_sts:batch_clf_sts, model.lm_y: batch_lm_y, model.clf_y: batch_clf_y, model.keep_prob: args.keep_prob}
+            feed_dict = {model.x: batch_x, model.xcola: batch_cola,model.xnli11: batch_xnli1, model.xnli2:batch_xnli2, model.clf_nli:batch_clf_nli, model.clf_sts:batch_clf_sts, model.lm_y: batch_lm_y, model.clf_y: batch_clf_y model.clf_cola:batch_clf_cola, model.keep_prob: args.keep_prob}
             _, step, summaries, total_loss, lm_loss, clf_loss,clf_loss_sts, clf_loss_nli = \
-                sess.run([train_op, global_step, summary_op, model.total_loss, model.lm_loss, model.clf_loss,model.clf_loss_sts,model.clf_loss_nli], feed_dict=feed_dict)
+                sess.run([train_op, global_step, summary_op, model.total_loss, model.lm_loss, model.clf_loss,model.clf_loss_sts,model.clf_loss_nli, model.clf_loss_cola], feed_dict=feed_dict)
             summary_writer.add_summary(summaries, step)
 
             if step % 100 == 0:
@@ -44,7 +44,7 @@ def train(train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_
             losses, accuracies, iters = 0, 0, 0
 
             for batch_x, batch_lm_y, batch_clf_y in test_batches:
-                feed_dict = {model.x: batch_x, model.xnli11: batch_xnli1, model.xnli2:batch_xnli2, model.clf_nli:batch_clf_nli, model.clf_sts:batch_clf_sts, model.lm_y: batch_lm_y, model.clf_y: batch_clf_y, model.keep_prob: args.keep_prob}
+                feed_dict = {model.x: batch_x, model.xcola:batch_cola,model.xnli11: batch_xnli1, model.xnli2:batch_xnli2, model.clf_nli:batch_clf_nli, model.clf_sts:batch_clf_sts, model.lm_y: batch_lm_y, model.clf_y: batch_clf_y,model.clf_cola :batch_clf_cola, model.keep_prob: args.keep_prob}
                 lm_loss, accuracy = sess.run([model.lm_loss, model.accuracy], feed_dict=feed_dict)
                 losses += lm_loss
                 accuracies += accuracy
@@ -53,7 +53,7 @@ def train(train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_
             print("\ntest perplexity = {0}".format(np.exp(losses / iters)))
             print("test accuracy = {0}\n".format(accuracies / iters))
 
-        batches = batch_iter(train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y, train_clf_sts, train_clf_nli, args.batch_size, args.num_epochs)
+        batches = batch_iter(train_x, train_xcola, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y, train_clf_sts, train_clf_nli, args.batch_size, args.num_epochs)
         for batch_x, batch_xnli1, batch_xnli2, batch_xsts1, batch_xsts2, batch_lm_y, batch_clf_y, batch_clf_sts, batch_clf_nli in batches:
             train_step(batch_x, batch_xnli1, batch_xnli2, batch_xsts1, batch_xsts2, batch_lm_y, batch_clf_y, batch_clf_sts, batch_clf_nli)
             step = tf.train.global_step(sess, global_step)
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     print("\nBuilding dictionary..")
     word_dict = build_word_dict()
     print("Preprocessing dataset..")
-    train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y , train_clf_sts, train_clf_nli = build_dataset("train", word_dict, args.max_document_len)
-    test_x, test_xnli1, test_xnli2, test_xsts1, test_xsts2, test_lm_y, test_clf_y , test_clf_sts, test_clf_nli  = build_dataset("test", word_dict, args.max_document_len)
+    train_x, train_cola,train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y , train_clf_sts, train_clf_nli,train_clf_cola = build_dataset("train", word_dict, args.max_document_len)
+    test_x, test_cola,test_xnli1, test_xnli2, test_xsts1, test_xsts2, test_lm_y, test_clf_y , test_clf_sts, test_clf_nli,test_clf_cola  = build_dataset("test", word_dict, args.max_document_len)
 
-    train(train_x, train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y, train_clf_nli, train_clf_sts, test_x, test_xnli1, test_xnli2, test_xsts1, test_xsts2, test_lm_y, test_clf_y,test_clf_nli, test_clf_sts ,word_dict, args)
+    train(train_x, train_cola,train_xnli1, train_xnli2, train_xsts1, train_xsts2, train_lm_y, train_clf_y, train_clf_nli,train_clf_cola, train_clf_sts, test_x, test_cola, test_xnli1, test_xnli2, test_xsts1, test_xsts2, test_lm_y, test_clf_y,test_clf_nli, test_clf_sts, test_clf_cola,word_dict, args)
